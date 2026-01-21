9cf845ea0de6b6d4db90abe8e6330bc3b4c2c5ee
haseeb <haseeeb8j@gmail.com>
2026-01-21 18:31:53 +0500

    added rofi shell

--- carbon/rofi/shell.py
+++ carbon/rofi/shell.py
@@ -1,1 +1,36 @@
 import subprocess
+from pathlib import Path
+from typing import overload
+
+
+
+class RofiShell:
+
+    class Mode():
+        drun = "drun"
+        run = "run"
+        window = "window"
+        filebrowser = "filebrowser"
+        ssh = "ssh"
+
+    @classmethod
+    def Run(cls, cmd: str) -> str:
+        output = subprocess.run(cmd, capture_output=True, text=True, shell=True)
+        if (output.returncode == 0): return output.stdout
+        return output.stderr
+
+
+    def __init__(self, rasi: str) -> None:
+        self.rasi = Path(rasi)
+
+
+    def displayMode(self, mode: str):
+        cmd = f"rofi -show {mode} -theme {self.rasi}"
+        self.Run(cmd)
+
+
+    def display(self, prompt: str, mesg: str, options: list[str]) -> str:
+        cmd = f"echo -e '{"\n".join(options)}' | rofi -dmenu -p {prompt} -mesg {mesg} -theme {self.rasi}"
+        return self.Run(cmd)
+    
+
