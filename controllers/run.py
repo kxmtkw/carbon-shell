7682f32ab8fb58d39ac52e48e77ea16f19baae1d
haseeb <haseeeb8j@gmail.com>
2026-02-22 22:54:37 +0500

    added history in run prompt

--- controllers/run.py
+++ controllers/run.py
@@ -3,13 +3,19 @@ import subprocess, shlex
 import os
 
 from lib.rofi import RofiShell
-
+import simpleeval
 
 class RunPrompt():
 	
 
 	def __init__(self):
 		self.rofi = RofiShell("~/.config/rofi/run/main.rasi")
+
+		self.hist_file: Path = Path("~/.carbon/cache/run_history.txt").expanduser()
+
+		
+		self.cache_file: Path = Path("~/.carbon/cache/run_cache.txt").expanduser()
+
 		self.options: list[str] = self.load_entries()
 
 
@@ -18,12 +24,63 @@ class RunPrompt():
 		print("Loading entries for run...")
 
 		options = []
-		options.extend(self.get_binaries())
+
+		if self.cache_file.exists():
+			print("Cache found! Using it instead.")
+			options.extend(self.load_cache())
+		else:
+			self.cache_file.touch()
+			options.extend(self.rebuild_cache())
+
+		if not self.hist_file.exists():
+			self.hist_file.touch()
+
+		options.extend(self.load_history())
 		
 		print("All entries loaded.")
 
 		return options
+	
 
+	def load_cache(self) -> list[str]:
+
+		with open(self.cache_file) as file:
+			content = file.read()
+
+		return content.splitlines()
+
+
+	def rebuild_cache(self) -> list[str]:
+		
+		print("Rebuilding cache...")
+
+		entries = []
+		entries.extend(self.get_binaries())
+
+		with open(self.cache_file, "w") as file:
+			file.write("\n".join(entries))
+
+		return entries
+	
+
+	def load_history(self) -> list[str]:
+
+		with open(self.hist_file, "r") as file:
+			content = file.read()
+
+		self.history_set = set(content.splitlines())
+		
+		history = list(self.history_set)
+
+		print("Loaded history.")
+
+		return history
+
+
+	def save_history(self, cmd: str):
+		with open(self.hist_file, "a") as file:
+			file.write(cmd+"\n")
+	
 
 	def get_binaries(self) -> list[str]:
 
@@ -51,10 +108,6 @@ class RunPrompt():
 		print("Loaded binaries.")
 
 		return binaries
-
-
-	def get_history(self) -> list[str]:
-		pass 
 
 	
 	def show_error(self, msg: str):
@@ -90,38 +143,49 @@ class RunPrompt():
 		
 		cmd = selected.strip()
 
-		mod = selected[0]
-		modded_cmd = selected[1:]
+		mod = cmd[0]
+		modded_cmd = cmd[1:]
 
 		match mod:
 			case "$":
 				self.execute_shell(modded_cmd)
-				return
-			
+			case "%":
+				self.parse(modded_cmd)
+			case "=":
+				self.execute_mathexpr(modded_cmd)
+			case _:
+				self.execute_subprocess(cmd)
 
-		self.execute_subprocess(cmd)
+		if mod != "%":
+			self.save_history(f"% {selected}")
 
 
 	def execute_subprocess(self, cmd: str):
 
+		
 		try:
 			cmd_list = shlex.split(cmd)
+			print(cmd_list)
 		except ValueError as e:
 			self.show_error(f"Syntax Error: {e}")
 			return
 
 		try:
-			subprocess.Popen(
+			proc = subprocess.Popen(
 				cmd_list,
 				stdin=subprocess.PIPE,
 				stdout=subprocess.PIPE
 			)
+			proc.wait(1)
 		except FileNotFoundError:
 			self.show_error(f"Command not found: {cmd}")
 			return
 		except PermissionError:
 			self.show_error("Insufficient Permissions!")
 			return
+		except subprocess.TimeoutExpired:
+			return
+		
 
 
 	def execute_shell(self, cmd: str):
@@ -132,6 +196,12 @@ class RunPrompt():
 			stdout=subprocess.PIPE,
 			shell=True
 		)
+
+	
+	def execute_mathexpr(self, expr: str):
+
+		result = simpleeval.simple_eval(expr)
+		self.show_error(str(result))
 
 
 if __name__ == "__main__":
