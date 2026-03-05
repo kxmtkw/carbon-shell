2673c20dd9ffe23f690ff2962715cf2a9e1926cc
haseeb <haseeeb8j@gmail.com>
2026-03-06 00:11:55 +0500

    updated: setup script and carbon bin scripts

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
+
+echo ">> Setting up Carbon Shell..."
+
+echo " :: Building python venv :: " 
 python3 -m venv .venv
 source ./.venv/bin/activate
-pip install .
-carbon install
 
-echo "Carbon shell sucessfully installed."
+echo " :: Installing dependancies :: " 
+pip install .
+
+echo ">> Carbon Shell setup complete!"
+
+echo "Note: To use and start the shell, source ~/.carbon/env in your shell config." \
+"Then run 'carbon.start' to start the shell."
