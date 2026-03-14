22c2a9a1128e67c1b2838004d6df6c9c0e8c2ed8
haseeb <haseeeb8j@gmail.com>
2026-03-15 00:05:58 +0500

    added: added hypr config back to shell for more better integration

---
+++ install.sh
@@ -0,0 +1,22 @@
+
+echo ">> Installing up Carbon Shell..."
+
+echo " :: Building environment " 
+python3 -m venv .venv
+source ./.venv/bin/activate
+
+echo " :: Installing dependancies " 
+pip install . > /dev/null 2>&1
+
+echo " :: Linking hyprland " 
+ln -s -i ~/.carbon/hypr ~/.config/hypr
+
+echo " :: Copying shell configuration "
+mkdir settings
+cp -i ~/.carbon/defaults/config.toml ~/.carbon/settings/config.toml
+cp -i ~/.carbon/defaults/colors.toml ~/.carbon/settings/colors.toml
+
+echo ">> Carbon Shell installation complete!"
+
+echo "Note: To use and start the shell, source ~/.carbon/env in your shell config." \
+"Then run 'carbon.start' to start the shell."
