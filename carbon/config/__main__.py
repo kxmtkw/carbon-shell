2442c067d71101ad46f4bf33746b2c13aa08bb52
haseeb <haseeeb8j@gmail.com>
2026-03-18 17:43:07 +0500

    fixed: implementation of silent arg

--- carbon/config/__main__.py
+++ carbon/config/__main__.py
@@ -1,9 +1,15 @@
 from .updater import ConfigUpdater
 from .getter import CarbonConfig
 
+import sys
+
 def main():
     updater = ConfigUpdater(CarbonConfig)
     updater.update()
+    
+    if len(sys.argv) > 1 and sys.argv[1] == "--silent":
+        return
+    
     updater.notify()
 
 if __name__ == "__main__":
