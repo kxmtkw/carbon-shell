1ef7898d683b08af8c0c8c5d508d13cd28860dd2
haseeb <haseeeb8j@gmail.com>
2026-03-21 22:55:14 +0500

    added: new controller networker to replace the old wifi one

---
+++ controllers/networker/interface.py
@@ -0,0 +1,233 @@
+from carbon.rofi import RofiShell
+import backend, nmcli
+
+class Networker:
+
+    def __init__(self):
+        self.rofi = RofiShell("~/.carbon/shell/rofi/wifi/main.rasi")
+        
+        self.is_running: bool = True
+
+        self.current = self.show_wifi_menu
+
+        self.wifi_device: str | None = backend.get_default_wifi_device()
+        self.selected_network: str | None = None
+
+
+    def launch(self):
+        while self.is_running:
+            self.current()
+        
+
+    def show_main_menu(self):
+        
+        title = "Networker"
+        mesg = ""
+        options = [
+            "List Devices",
+            "Configure Wifi",
+            "Configure Hotspot",
+            "Airplane Mode",
+            "Advanced Settings"
+        ]
+
+        self.rofi.display(
+            mode=RofiShell.Mode.dmenu,
+            prompt=title,
+            mesg=mesg,
+            options=options
+        )
+
+        selected = self.rofi.wait()
+
+        if not selected: 
+            exit()
+
+        elif selected == options[0]:
+            self.current = self.show_device_menu
+
+        elif selected == options[1]:
+            self.current = self.show_wifi_menu
+
+        elif selected == options[2]:
+            self.current = self.show_hotspot_menu
+
+        elif selected == options[3]:
+            backend.toggle_all_radio()
+
+        elif selected == options[4]:
+            self.current = self.show_device_menu
+            
+
+    def show_device_menu(self):
+        
+        title = "Network Devices"
+        mesg = ""
+        options = backend.list_devices()
+
+        self.rofi.display(
+            mode=RofiShell.Mode.dmenu,
+            prompt=title,
+            mesg=mesg,
+            options=options
+        )
+
+        selected = self.rofi.wait()
+
+        if not selected: 
+            exit()
+
+        else:
+            self.current = self.show_main_menu
+
+
+    def show_hotspot_menu(self):
+        # todo
+        self.current = self.show_main_menu
+
+
+    def show_wifi_menu(self):
+
+        self.rofi.updateTheme("~/.carbon/shell/rofi/wifi/main.rasi")
+        
+        title = "Wifi"
+        mesg = f"Device: {self.wifi_device}"
+        options = [
+            ">> Toggle Wifi",
+            ">> Rescan",
+        ]
+
+        networks = backend.list_networks(self.wifi_device, True)
+
+        options.extend(
+            networks
+        )
+
+        options.extend([
+            ">> Change Device",
+            ">> Networker Menu"    
+        ])
+
+        self.rofi.display(
+            mode=RofiShell.Mode.dmenu,
+            prompt=title,
+            mesg=mesg,
+            options=options
+        )
+
+        selected = self.rofi.wait()
+
+        if not selected: 
+            exit()
+
+        elif selected == options[0]:
+            backend.toggle_wifi_radio()
+            backend.list_networks(self.wifi_device, True)
+        
+        elif selected == options[1]:
+            backend.list_networks(self.wifi_device, True)
+
+        elif selected == options[-2]:
+            self.current = self.show_wifi_device_options
+
+        elif selected == options[-1]:
+            self.current = self.show_main_menu
+
+        else:
+            self.selected_network = backend.get_id_from_formatted(selected)
+            self.current = self.show_wifi_network_options
+
+
+    def show_wifi_network_options(self):
+        
+        network = backend.get_network(self.selected_network)
+
+        title = "Wifi Network"
+        mesg = network.ssid
+        options = [
+            "Return",
+            "Disconnect" if network.in_use else "Connect",
+            "Forget"
+        ]
+
+        self.rofi.display(
+            mode=RofiShell.Mode.dmenu,
+            prompt=title,
+            mesg=mesg,
+            options=options
+        )
+
+        selected = self.rofi.wait()
+
+        if not selected: 
+            exit()
+
+        elif selected == options[0]:
+            self.current = self.show_wifi_menu
+        
+        elif selected == options[1]:
+            if network.in_use:
+                backend.disconnect_network(network.bssid)
+            else:
+                try:
+                    backend.connect_network(self.wifi_device, network.bssid)
+                except backend.Errors.ConnectionFailure:
+                    self.current = self.show_password_prompt
+                    return
+
+        elif selected == options[2]:
+            backend.forget_network(network.bssid)
+
+        self.current = self.show_wifi_menu
+
+
+    def show_wifi_device_options(self):
+        title = "Wifi Devices"
+        mesg = ""
+        options = backend.list_wifi_devices()
+
+        self.rofi.display(
+            mode=RofiShell.Mode.dmenu,
+            prompt=title,
+            mesg=mesg,
+            options=options
+        )
+
+        selected = self.rofi.wait()
+
+        if not selected: 
+            exit()
+
+        else:
+            self.wifi_device = backend.get_id_from_formatted(selected)
+            self.current = self.show_wifi_menu
+
+
+    def show_password_prompt(self):
+        self.rofi.updateTheme("~/.carbon/shell/rofi/wifi/password.rasi")
+        title = "Password Required"
+        mesg = ""
+
+        self.rofi.display(
+            mode=RofiShell.Mode.dmenu,
+            prompt=title,
+            mesg=mesg,
+        )
+
+        selected = self.rofi.wait()
+
+        if not selected:
+            exit()
+
+        try:
+            backend.connect_network(self.wifi_device, self.selected_network, selected)
+        except backend.Errors.AuthFailure as e:
+            raise e
+        
+        self.current = self.show_wifi_menu
+        
+
+    def show_error_mesg(self):
+        pass
+
+    
---
+++ controllers/networker/main.py
@@ -0,0 +1,5 @@
+from interface import Networker
+
+if __name__ == "__main__":
+    c = Networker()
+    c.launch()
