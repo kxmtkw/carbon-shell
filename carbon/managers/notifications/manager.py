d8ce0af5111f4009bbdc20e7e488b7170329b52e
haseeb <haseeeb8j@gmail.com>
2026-04-11 22:26:30 +0500

    added: dnd mode

--- carbon/managers/notifications/manager.py
+++ carbon/managers/notifications/manager.py
@@ -1,40 +1,58 @@
 import asyncio
+from dataclasses import dataclass, replace
 from threading import Lock, Thread
-from typing import Any, Callable, Dict
+from typing import Any, Callable, Dict, Literal
 from dbus_next.service import ServiceInterface, method, signal
 from dbus_next.aio import MessageBus
 from dbus_next import Variant
 
 from carbon.managers.base import BaseManager
-from carbon.utils import locked, logger
+from carbon.utils import locked, logger, CarbonError
 
 from carbon.lib.quickshell import Quickshell
 
 from .server import NotificationServer, Notification
+
+
+
 
 notificationLock = Lock()
 
 class NotificationManager(BaseManager):
     
 
+    @dataclass(init=True, kw_only=True)
+    class State(BaseManager.State):
+        do_not_disturb: bool
+
+
     def __init__(self):
         super().__init__()
         self.server: NotificationServer
+        self.notifications: list[Notification] = []
+
         self.daemon_thread = Thread(target=self.startServer, daemon=True)
         self.daemon_thread.start()
+
         self.quickshell = Quickshell()
+
+        self.state = NotificationManager.State(
+            do_not_disturb=False
+        )
         
 
     def handlers(self) -> Dict[str, Callable]:
-        return {}
+        return {
+            "dnd": self.setDND
+        }
     
 
-    def saveState(self) -> dict[str, Any]:
-        return
+    def getState(self) -> dict[str, Any]:
+        return replace(self.state)
     
 
-    def loadState(self, state: dict[str, Any]):
-        return
+    def setState(self, state: State):
+        self.setDND(state.do_not_disturb)
     
 
     def startServer(self):
@@ -65,17 +83,7 @@ class NotificationManager(BaseManager):
     @locked(notificationLock)
     def newNotification(self, notif: Notification):
         
-        self.quickshell.sendNotification(
-            notif.id,
-            notif.replaces_id,
-            notif.app_name,
-            notif.app_icon,
-            notif.summary,
-            notif.body,
-            notif.urgency,
-            notif.image,
-            notif.expire_timeout
-        )
+        self.notifications.append(notif)
 
         logger.log(
             "notifications",
@@ -88,3 +96,58 @@ class NotificationManager(BaseManager):
             f"Notification info for #{notif.id}: ({notif.app_name}) ({notif.app_icon}) ({notif.summary}) ({notif.body if len(notif.body) > 20 else f'{notif.body[:17]}...'}) ({notif.image}) ({notif.urgency})",
             logger.Level.debug
         )
+
+        if self.state.do_not_disturb:
+            logger.log(
+                "notifications",
+                f"Not dispatching notification #{notif.id} due to DND",
+                logger.Level.debug
+            )
+            return
+        
+
+        while len(self.notifications) > 0:
+            n = self.notifications.pop()
+            self.quickshell.sendNotification(
+                n.id,
+                n.replaces_id,
+                n.app_name,
+                n.app_icon,
+                n.summary,
+                n.body,
+                n.urgency,
+                n.image,
+                n.expire_timeout
+            )
+
+            logger.log(
+                "notifications",
+                f"Dispatching notification #{n.id}.",
+                logger.Level.debug
+            )
+        
+
+    @locked(notificationLock)
+    def setDND(self, *, state: Literal["on", "off", "toggled"]):
+
+        if state == "on":
+            self.state.do_not_disturb = True
+        elif state == "off":
+            self.state.do_not_disturb = False
+        elif state == "toggle":
+            self.state.do_not_disturb = not self.state.do_not_disturb
+        else:
+            raise CarbonError(f"Invalid state: {state}. Valid values are: on, off, toggle.")
+        
+        if self.state.do_not_disturb:
+            msg = "DND now on. Notifications will be hidden until it is turned on."
+        else:
+            msg = "DND now off. Queued Notifications will now be shown."
+
+        logger.log(
+            "notifications",
+            msg,
+            logger.Level.info
+        )
+
+        return msg
