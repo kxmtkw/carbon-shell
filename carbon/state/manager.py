e71ffe8c5286441c5cc22c654afd3828ee1f57ca
haseeb <haseeeb8j@gmail.com>
2026-04-16 22:47:09 +0500

    updated: better error reporting if config is incorrect

--- carbon/state/manager.py
+++ carbon/state/manager.py
@@ -1,21 +1,25 @@
 from pathlib import Path
 import json
-from typing import Any
+from typing import Any, Callable
+
+from carbon.utils import FileWatcher, notify
 
 class StateManager:
 
-    def __init__(self):
+    def __init__(self, path: str):
         self._state = {}
-        self._state_file = Path("~/.carbon/user/state.json").expanduser()
-
-        if self._state_file.exists(): return
+        self._state_file = Path(path).expanduser()
+        self._is_loading_needed = True
 
         if not self._state_file.parent.exists():
             self._state_file.parent.mkdir(511, True, True)
 
-        with open(self._state_file, "w") as file:
-            json.dump({}, file)
+            with open(self._state_file, "w") as file:
+                json.dump({}, file)
 
+    @property
+    def file(self) -> Path:
+        return self._state_file
 
     def update(self, key: str, state: dict):
         self._state[key] = state
@@ -26,21 +30,20 @@ class StateManager:
     
 
     def save(self):
-
+        
         string = json.dumps(self._state, skipkeys=True, indent=4) 
                 
         with open(self._state_file, "w") as file:
             file.write(string)
            
 
-    def load(self):
+    def load(self) -> bool:
+        
         with open(self._state_file) as file:
             try:
                 data = json.load(file)
             except json.JSONDecodeError:
-                data = {}
+                return False
 
         self._state = data
-
-
-
+        return True
