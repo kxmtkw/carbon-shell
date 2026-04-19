4290a20b694f3a92c9b40c4f3d35ae2bd8dabb7f
haseeb <haseeeb8j@gmail.com>
2026-04-19 13:22:13 +0500

    updated: better process management

--- carbon/managers/nightlight/manager.py
+++ carbon/managers/nightlight/manager.py
@@ -4,7 +4,7 @@ from typing import Any
 import time
 
 from carbon.managers.base import BaseManager
-from carbon.utils import shellrun, logger, CarbonError
+from carbon.utils import shellrun, logger, CarbonError, ProcessManager
 
 
 
@@ -19,13 +19,17 @@ class NightLightManager(BaseManager):
 
     def __init__(self):
         super().__init__()
+        self.default_temperature = 6000
+        self.default_gamma = 100
+
         self.state = NightLightManager.State(
-            temperature=5800,
-            gamma=100,
+            temperature=self.default_temperature,
+            gamma=self.default_gamma,
             toggled=True
         )
 
-    
+        self.hyprsunset = ProcessManager("hyprsunset")
+
     def handlers(self):
         return {
             "on": lambda: self.toggleNightlight(True),
@@ -42,19 +46,19 @@ class NightLightManager(BaseManager):
 
     def setState(self, state: State):
         self.toggleNightlight(state.toggled)
-        self.setTemperature(state.temperature)
-        self.setGamma(state.gamma)
 
+        
 
     def toggleNightlight(self, on: bool):
 
         self.state.toggled = on
-
+            
         if self.state.toggled:
-            shellrun(f"pidof hyprsunset || hyprsunset -g {self.state.gamma} -t {self.state.temperature}", wait=False)
+            self.hyprsunset.start("-g", self.state.gamma, "-t", self.state.temperature)
+            print("Started")
             return "Nightlight turned on"
         else:
-            shellrun("pkill hyprsunset")
+            self.hyprsunset.kill()
             return "Nightlight turned off"
 
 
@@ -62,6 +66,10 @@ class NightLightManager(BaseManager):
 
         if value < 1000 or value > 20000:
             raise CarbonError("Invalid temperature value. Valid range: 1000-20000.")
+        
+        if not self.state.toggled:
+            return "Updated, but nightlight is off."
+        
         success, output = shellrun(f"hyprctl hyprsunset temperature {value}")
 
         if not success:
@@ -73,13 +81,16 @@ class NightLightManager(BaseManager):
             logger.Level.info
         )
         self.state.temperature = value
-        return "Updated."
+        return "Updated temperature."
 
 
     def setGamma(self, value: int) -> str:
 
         if value < 10 or value > 200:
             raise CarbonError("Invalid gamma value. Valid range: 10-200.")
+        
+        if not self.state.toggled:
+            return "Updated, but nightlight is off."
         
         success, output = shellrun(f"hyprctl hyprsunset gamma {value}")
 
@@ -92,6 +103,5 @@ class NightLightManager(BaseManager):
             logger.Level.info
         )
         self.state.gamma = value
-        return "Updated."
-    
+        return "Updated gamma."
 
