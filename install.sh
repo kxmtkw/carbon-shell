54f151d3f9881dc486942a0b58922693dc9d1a03
Haseeb <haseeeb8j@gmail.com>
2026-05-01 19:22:23 +0500

    fixed: clone command to use 'git clone'

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 [ -d ~/.carbon ] && rm -rf ~/.carbon
-clone https://github.com/kxmtkw/carbon-shell.git ~/.carbon
+git clone https://github.com/kxmtkw/carbon-shell.git ~/.carbon
 python3 ~/.carbon/installation/install.py
