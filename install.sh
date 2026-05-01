418a5a18eff60fdd26f3646e128f8dd602f5a085
haseeb <haseeeb8j@gmail.com>
2026-05-01 19:33:29 +0500

    fixed: insallation

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-[ -d ~/.carbon ] && rm -rf ~/.carbon
+[ -d ~/.carbon ] && echo "~/.carbon already exists" && exit 1
 git clone https://github.com/kxmtkw/carbon-shell.git ~/.carbon
 python3 ~/.carbon/installation/install.py
