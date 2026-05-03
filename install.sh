d10e59447f1f8c0146f197fc27304000978f6620
haseeb <haseeeb8j@gmail.com>
2026-05-03 22:25:45 +0500

    removed: env file; updated: docs and install script, made installation more easier

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-[ -d ~/.carbon ] && echo "~/.carbon already exists" && exit 1
+[ -d ~/.carbon ] && echo "~/.carbon already exists. Delete it first before installing." && exit 1
 git clone https://github.com/kxmtkw/carbon-shell.git ~/.carbon
 cd ~/.carbon
 python3 ~/.carbon/installation/install.py
